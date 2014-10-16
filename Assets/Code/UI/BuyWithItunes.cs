b8d4c119da9d9cdbb99821c313d3175998368f32
Adrian <adrianmayprogrammer@gmail.com>
2014-10-16 16:15:21 +0100

    Prettying up of IAPs

--- Assets/Code/UI/BuyWithItunes.cs
+++ Assets/Code/UI/BuyWithItunes.cs
@@ -3,10 +3,6 @@ using System.Collections;
 
 public class BuyWithItunes : MonoBehaviour 
 {
-	void Start()
-	{
-		RegisterListeners();
-	}
 	void OnClick()
 	{
 		Debug.Log("Buying with itunes \n Opening IAP");
@@ -19,31 +15,5 @@ public class BuyWithItunes : MonoBehaviour
 		}
 		ProfilesManagementScript.Singleton.LoadingSpinner.SetActive(true);
 		ProfilesManagementScript.Singleton.PurchaseChoiceScreen.SetActive(false);
-	}
-
-	void purchaseSuccessful( StoreKitTransaction transaction )
-	{
-		ProfilesManagementScript.Singleton.LoadingSpinner.SetActive(false);
-		ProfilesManagementScript.Singleton.AniminsScreen.SetActive(true);
-		UnregisterListeners();
-	}
-	void purchaseUnsuccessful( string response )
-	{
-		Debug.Log("Purchase Unsuccessful, response: " + response);
-		ProfilesManagementScript.Singleton.LoadingSpinner.SetActive(false);
-		ProfilesManagementScript.Singleton.AniminsScreen.SetActive(true);
-		UnregisterListeners();
-	}
-	void RegisterListeners()
-	{
-		if(Application.isEditor){ return; }
-		StoreKitManager.purchaseSuccessfulEvent += purchaseSuccessful;
-		StoreKitManager.purchaseFailedEvent += purchaseUnsuccessful;
-	}
-	void UnregisterListeners()
-	{
-		if(Application.isEditor){ return; }
-		StoreKitManager.purchaseSuccessfulEvent -= purchaseSuccessful;
-		StoreKitManager.purchaseFailedEvent -= purchaseUnsuccessful;
 	}
 }
---
+++ Assets/Code/UI/ItunesButtonUpdate.cs
@@ -0,0 +1,127 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItunesButtonUpdate : MonoBehaviour 
+{
+	[SerializeField]
+	private UISprite mCharacterIcon;
+	[SerializeField]
+	private UILabel mCharacterTitle;
+	[SerializeField]
+	private UISprite[] mIcons;
+	[SerializeField]
+	private GameObject mPiHeader;
+	[SerializeField]
+	private GameObject mTboHeader;
+	[SerializeField]
+	private GameObject mKelseyHeader;
+	[SerializeField]
+	private GameObject mMandiHeader;
+
+
+
+	private Color piColourLight = new Color(206,255,82);
+	private Color piColourDark = new Color(42,196,10);
+	private Color tboColourLight = new Color(201,224,255);
+	private Color tboColourDark = new Color(123,196,244);
+	private Color kelseyColourLight = new Color(255,170,239);
+	private Color kelseyColourDark = new Color(67,57,123);
+	private Color mandiColourLight = new Color(255,255,255);
+	private Color mandiColourDark = new Color(252,251,224);
+
+	private string mName;
+	private int mId;
+
+	void Start()
+	{
+		RegisterListeners();
+		mCharacterTitle = new UILabel();
+	}
+
+	void OnEnable()
+	{
+		mCharacterTitle = new UILabel();
+		mCharacterIcon = new UISprite();
+		SetCharacterIcons();
+		mCharacterIcon.gameObject.SetActive(true);
+		mName = mCharacterIcon.name;
+
+		switch(mId)
+		{
+		case 0:
+			mPiHeader.SetActive(true);
+			break;
+		case 1:
+			mTboHeader.SetActive(true);
+			break;
+		case 2:
+			mKelseyHeader.SetActive(true);
+			break;
+		case 3:
+			mMandiHeader.SetActive(true);
+			break;
+		default:
+			break;
+		}
+	}
+	void OnDisable()
+	{
+		mPiHeader.SetActive(false);
+		mTboHeader.SetActive(false);
+		mKelseyHeader.SetActive(false);
+		mMandiHeader.SetActive(false);
+		mCharacterIcon.gameObject.SetActive(false);
+	}
+
+	void SetCharacterIcons()
+	{
+		int code = (int)ProfilesManagementScript.Singleton.AniminToUnlockId;
+		switch(code)
+		{
+		case 0:
+			mName = "Pi";
+			break;
+		case 1:
+			mName = "Tbo";
+			break;
+		case 2:
+			mName = "Kelsey";
+			break;
+		case 3:
+			mName = "Mandi";
+			break;
+		default:
+			break;
+
+		}
+		mId = (int)ProfilesManagementScript.Singleton.AniminToUnlockId;
+		mCharacterIcon = mIcons[mId];
+		mCharacterTitle.text = mName;
+	}
+
+	void purchaseSuccessful( StoreKitTransaction transaction )
+	{
+		ProfilesManagementScript.Singleton.LoadingSpinner.SetActive(false);
+		ProfilesManagementScript.Singleton.AniminsScreen.SetActive(true);
+		UnregisterListeners();
+	}
+	void purchaseUnsuccessful( string response )
+	{
+		Debug.Log("Purchase Unsuccessful, response: " + response);
+		ProfilesManagementScript.Singleton.LoadingSpinner.SetActive(false);
+		ProfilesManagementScript.Singleton.AniminsScreen.SetActive(true);
+		UnregisterListeners();
+	}
+	void RegisterListeners()
+	{
+		if(Application.isEditor){ return; }
+		StoreKitManager.purchaseSuccessfulEvent += purchaseSuccessful;
+		StoreKitManager.purchaseFailedEvent += purchaseUnsuccessful;
+	}
+	void UnregisterListeners()
+	{
+		if(Application.isEditor){ return; }
+		StoreKitManager.purchaseSuccessfulEvent -= purchaseSuccessful;
+		StoreKitManager.purchaseFailedEvent -= purchaseUnsuccessful;
+	}
+}
