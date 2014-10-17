5b8d2f885f70d5de2ad89c53bf2dec70fc048f9a
DirtyHarryE <harridunc@gmail.com>
2014-10-17 16:33:37 +0100

    Added missing files. Including all the sounds.

---
+++ Assets/Code/Harrys/BetweenSceneData.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BetweenSceneData
+{
+
+    private bool m_MiniGameCelebration;
+    public bool MiniGameCelebration
+    {
+        get
+        {
+            return m_MiniGameCelebration;
+        }
+        set
+        {
+            m_MiniGameCelebration = value;
+        }
+    }
+
+    public void DoCelebration(){
+
+        Debug.Log("Minigame Celebration test : [" + m_MiniGameCelebration + "];");
+        if (m_MiniGameCelebration)
+        {
+            
+        }
+    }
+
+
+    #region Singleton
+    static BetweenSceneData m_Instance;
+    public static BetweenSceneData Instance
+    {
+        get
+        {
+            if (m_Instance == null)
+                m_Instance = new BetweenSceneData();
+            return m_Instance;
+        }
+    }
+    #endregion
+}
