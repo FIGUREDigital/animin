45c982827bb4947b5f6cb76f069244a6a3253c8d
Adrian <adrianmayprogrammer@gmail.com>
2014-10-26 14:34:36 +0000

    Merge branch 'master' of https://github.com/FIGUREDigital/animin
    Conflicts:
    	Assets/Scenes/VuforiaTest.unity

---
+++ Assets/Code/Scripts/OpenShop.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenShop : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		UnlockCharacterManager.Instance.OpenShop();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
