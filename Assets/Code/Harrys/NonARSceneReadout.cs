7b93eda63bcf496ebe340c861fa219a2c7d0f189
Harry Evans <harridunc@gmail.com>
2014-10-27 17:12:08 +0000

    Tweaked loading menu. Tweaking NonARScene readout

--- Assets/Code/Harrys/NonARSceneReadout.cs
+++ Assets/Code/Harrys/NonARSceneReadout.cs
@@ -6,16 +6,18 @@ public class NonARSceneReadout : MonoBehaviour {
 	UILabel m_label;
 	// Use this for initialization
 	void Start () {
-		m_label = this.GetComponent<UILabel> ();
+		if (Debug.isDebugBuild) {
+			gameObject.SetActive(false);
+		} else {
+			m_label = this.GetComponent<UILabel> ();
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Debug.isDebugBuild) {
+		if (gameObject.activeInHierarchy) {
 			bool isActive = UIGlobalVariablesScript.Singleton.NonARWorldRef.activeInHierarchy;
 			m_label.text = ("NonARScene : [" + (isActive ? "enabled" : "disabled") + "]");
-		} else {
-			m_label.text = ("");
 		}
 	}
 }
