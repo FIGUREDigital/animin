88a0adb4b4beee2f0d974e072872a223ba883d40
Harry Evans <harridunc@gmail.com>
2014-10-27 16:28:39 +0000

    Console readout for NonARScene

---
+++ Assets/Code/Harrys/NonARSceneReadout.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NonARSceneReadout : MonoBehaviour {
+	
+	UILabel m_label;
+	// Use this for initialization
+	void Start () {
+		m_label = this.GetComponent<UILabel> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool isActive = UIGlobalVariablesScript.Singleton.NonARWorldRef.activeInHierarchy;
+		m_label.text = ("NonARScene : ["+ (isActive?"enabled":"disabled") + "]");
+	}
+}
