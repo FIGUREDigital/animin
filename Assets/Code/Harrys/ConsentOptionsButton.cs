fbd1b2375abf42b4b2b4b94d7232b48f30f10569
Harry Evans <harridunc@gmail.com>
2014-10-28 18:29:13 +0000

    New buttons on settings menu. Settings menu now saved as prefab.

---
+++ Assets/Code/Harrys/ConsentOptionsButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConsentOptionsButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnClick(){
+		Debug.Log ("As a young boy, Brutus' mother often cautioned him not to run with Caesars.");
+	}
+}
