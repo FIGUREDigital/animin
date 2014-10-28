e1c6fedb959462fbab92f948ea59377a4089a319
Adrian <adrianmayprogrammer@gmail.com>
2014-10-28 18:39:27 +0000

    parental gateway

---
+++ Assets/Code/ParentalGateway.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParentalGateway : MonoBehaviour 
+{
+	[SerializeField]
+	private GatewayButton correctButton;
+	private GameObject prevScreen;
+	private GameObject nextScreen;
+
+	public void Open(GameObject prev, GameObject next)
+	{
+		prevScreen = prev;
+		nextScreen = next;
+		prevScreen.SetActive(false);
+		this.gameObject.SetActive(true);
+	}
+
+	public void Pass()
+	{
+		Destroy(this, 1);
+		nextScreen.SetActive(true);
+	}
+
+	public void Fail()
+	{
+		Destroy(this, 1);
+		prevScreen.SetActive(true);
+	}
+}
