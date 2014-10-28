f5e6a30c7fbb662a9e8b4d8a35a262ad4cda81f7
Harry Evans <harridunc@gmail.com>
2014-10-28 21:04:39 +0000

    Parental controls added, though without functionality.

--- Assets/Code/Harrys/UI/Parental Controls/ParentalControls.cs
+++ Assets/Code/Harrys/UI/Parental Controls/ParentalControls.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ConsentOptionsButton : MonoBehaviour {
+public class ParentalControls : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -11,8 +11,5 @@ public class ConsentOptionsButton : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
-	}
-	void OnClick(){
-		Debug.Log ("As a young boy, Brutus' mother often cautioned him not to run with Caesars.");
 	}
 }
