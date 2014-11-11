f575fb831149b41f0f3d7800e41bd3fa7ecd9c1d
Adrian <adrianmayprogrammer@gmail.com>
2014-11-11 16:58:37 +0000

    only one email

--- Assets/Code/UI/SendAddress.cs
+++ Assets/Code/UI/SendAddress.cs
@@ -3,12 +3,21 @@ using System.Collections;
 
 public class SendAddress : MonoBehaviour 
 {
+	AddressScreen screen;
 	void OnClick()
 	{
-
-		AddressScreen screen = transform.parent.GetComponent<AddressScreen>();
-		screen.Send();
+		screen = transform.parent.GetComponent<AddressScreen>();
+		if(!Application.isEditor)
+		{
+			StartCoroutine("SendEmail");
+		}
 		ProfilesManagementScript.Singleton.AddressInput.SetActive(false);
 		ProfilesManagementScript.Singleton.AniminsScreen.SetActive(true);
+	}
+
+	private IEnumerator SendEmail()
+	{
+		screen.Send();
+		return null;
 	}
 }
