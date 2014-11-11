b86670892b333afb4fe08bbc404d621d34dc3635
Adrian <adrianmayprogrammer@gmail.com>
2014-11-11 17:03:39 +0000

    Revert "only one email"
    This reverts commit f575fb831149b41f0f3d7800e41bd3fa7ecd9c1d.

--- Assets/Code/UI/SendAddress.cs
+++ Assets/Code/UI/SendAddress.cs
@@ -3,21 +3,12 @@ using System.Collections;
 
 public class SendAddress : MonoBehaviour 
 {
-	AddressScreen screen;
 	void OnClick()
 	{
-		screen = transform.parent.GetComponent<AddressScreen>();
-		if(!Application.isEditor)
-		{
-			StartCoroutine("SendEmail");
-		}
+
+		AddressScreen screen = transform.parent.GetComponent<AddressScreen>();
+		screen.Send();
 		ProfilesManagementScript.Singleton.AddressInput.SetActive(false);
 		ProfilesManagementScript.Singleton.AniminsScreen.SetActive(true);
-	}
-
-	private IEnumerator SendEmail()
-	{
-		screen.Send();
-		return null;
 	}
 }
