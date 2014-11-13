b6dde5838417bfd6df95ba9dcef07bf1e498d599
Adrian <adrianmayprogrammer@gmail.com>
2014-11-13 17:16:32 +0000

    error box

---
+++ Assets/Code/messageChanger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class messageChanger : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		string platform;
+#if UNITY_IOS
+		platform = "iTunes";
+#elif UNITY_ANDROID
+		platform = "Play";
+#endif
+		string body = "We're having problems connecting to the " + platform + " store at the moment, please try again later or turn on wi-fi.";
+		gameObject.GetComponent<UILabel>().text = body;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
